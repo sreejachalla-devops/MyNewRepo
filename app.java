a8529de73f1031dcd9b1d68e8db9cf70e4cd93b4
sreejachalla-devops <challasreeja5@gmail.com>
2025-12-03 16:34:33 -0500

    Update app.java

--- app.java
+++ app.java
@@ -3,3 +3,17 @@ public class HelloWorld {
         System.out.println("Hello, World!");
     }
 }
+
+//dev-2 push from local
+public class BasicMath {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulus: " + (a % b)); // Remainder
+    }
+}
